027e4c0ab8b38e1c8530036fd0382947de834f64
Hermes <hermes.frangoudis@gmail.com>
2020-05-15 21:31:13 -0400

    updated rtm, added model positioning

--- js/webar-audience-client.js
+++ js/webar-audience-client.js
@@ -30,56 +30,14 @@ client.on('stream-added', (evt) => {
   });
 
   streamCount++;
-  // create video element
-  var video = document.createElement('video');
-  video.id = "faceVideo-" + streamId;
-  video.setAttribute('webkit-playsinline', 'webkit-playsinline');
-  video.setAttribute('playsinline', 'playsinline');
-  // add video object to the DOM
-  document.querySelector("a-assets").appendChild(video);
-
-  // add the new broadcaster
-  const gltfModel = "#broadcaster";
-  const scale = "-0.55 -0.55 -0.55"; // invert UVs (hack)
-  const offset = (streamCount-1);
-  const position = offset + " 0 0";
-  const rotation = "180 0 0";
-
-  const parent = document.querySelector('a-marker');
-  var newBroadcaster = document.createElement('a-gltf-model');
-  newBroadcaster.setAttribute('id', streamId);
-  newBroadcaster.setAttribute('gltf-model', gltfModel);
-  newBroadcaster.setAttribute('scale', scale);
-  newBroadcaster.setAttribute('position', position);
-  newBroadcaster.setAttribute('rotation', rotation);
-  parent.appendChild(newBroadcaster);
-
-  console.log(newBroadcaster);
-
-  newBroadcaster.addEventListener('model-loaded', () => {
-    var mesh = newBroadcaster.getObject3D('mesh');
-    mesh.traverse((node) => {
-      // search the mesh's children for the face-geo
-      if (node.isMesh && node.name == 'face-geo') {
-        // create video texture from video element
-        var texture = new THREE.VideoTexture(video);
-        texture.minFilter = THREE.LinearFilter; 
-        texture.magFilter = THREE.LinearFilter; 
-        texture.flipY = false;
-        // set node's material map to video texture
-        node.material.map = texture
-        node.material.color = new THREE.Color();
-        node.material.metalness = 0;
-      }
-    });
-  }); 
+  createBroadcaster(streamId);  // create 3d broadcaster
 });
 
 client.on('stream-removed', (evt) => {
   const stream = evt.stream;
   stream.stop(); // stop the stream
   stream.close(); // clean up and close the camera stream
-  console.log("Remote stream is removed " + stream.getId());
+  console.log('Remote stream is removed ' + stream.getId());
 });
 
 client.on('stream-subscribed', (evt) => {
@@ -88,12 +46,8 @@ client.on('stream-subscribed', (evt) => {
   console.log('Successfully subscribed to remote stream: ' + remoteStream.getId());
   
   // get the designated video element and add the stream as its video source
-  var video = document.getElementById("faceVideo-" + remoteId);
-  video.srcObject = remoteStream.stream;
-  video.onloadedmetadata = () => {
-    video.play();
-    console.log("::::::: ready to play video ::::::::::::::");
-  }
+  var video = document.getElementById('faceVideo-' + remoteId);
+  connectStreamToVideo(remoteStream, video)
 
 });
 
@@ -103,26 +57,26 @@ client.on('peer-leave', (evt) => {
   evt.stream.stop(); // stop the stream
   const remoteId = evt.stream.getId();
   document.getElementById(remoteId).remove();
-  document.getElementById("faceVideo-" + remoteId);
+  document.getElementById('faceVideo-' + remoteId);
   streamCount--;
 });
 
 // show mute icon whenever a remote has muted their mic
 client.on('mute-audio', (evt) => {
-  console.log("mute-audio for: " + evt.uid);
+  console.log('mute-audio for: ' + evt.uid);
 });
 
 client.on('unmute-audio', (evt) => {
-  console.log("unmute-audio for: " + evt.uid);
+  console.log('unmute-audio for: ' + evt.uid);
 });
 
 // show user icon whenever a remote has disabled their video
 client.on('mute-video', (evt) => {
-  console.log("mute-video for: " + evt.uid);
+  console.log('mute-video for: ' + evt.uid);
 });
 
 client.on('unmute-video', (evt) => {
-  console.log("unmute-video for: " + evt.uid);
+  console.log('unmute-video for: ' + evt.uid);
 });
 
 // join a channel
@@ -165,32 +119,15 @@ rtmClient.on('ConnectionStateChange', (newState, reason) => {
 rtmChannel.on('ChannelMessage', ({ text }, senderId) => { 
   // text: text of the received channel message; senderId: user ID of the sender.
   console.log('AgoraRTM msg from user ' + senderId + ' recieved: \n' + text);
-
-  var msg = text.split("-");
-  const cmd = msg[0];
-  const state = msg[1];
-  if (cmd === 'rotation') {
-    console.log('cmd found')
-    var model = document.getElementById(senderId);
-    if (state === 'start'){
-      console.log(state + ' : state')
-      const direction = msg[2];
-      rotateModel(model, direction) 
-    } else if (state === 'end') {
-      console.log(state + ' : state')
-      modelRotation = msg[2];
-      model.object3D.rotation.y = modelRotation
-    }
+  // convert from string to JSON
+  const msg = JSON.parse(text); 
+  console.log(msg)
+  // Handle RTM msg 
+  if (msg.action == 'rotation') {
+    rotateModel(senderId, msg.direction)
+  } else if (msg.action == 'position') {
+    moveModel(senderId, msg.direction)
   }
-
-  // const msg = JSON.parse(text);
-  // const property = msg.property
-  // const attributeValue = msg.attributeValue
-  // console.log(property);
-  // console.log(attributeValue);
-  // var model = document.getElementById(senderId);
-  // // model.object3D[property] = attributeValue
-  // model.setAttribute(property, attributeValue);
 });
 
 function joinRTMChannel(uid){
@@ -210,12 +147,33 @@ function joinRTMChannel(uid){
   });
 }
 
-function rotateModel(model, direction) {
-  if (direction === 'positive') {
+function rotateModel(uid, direction) {
+  var model = document.getElementById(uid)
+  if (direction === 'counter-clockwise') {
     model.object3D.rotation.y += 0.1;
-  } else if (direction === 'negative') {
+  } else if (direction === 'clockwise') {
     model.object3D.rotation.y -= 0.1;
   }  
+}
+
+function moveModel(uid, direction) {
+  var model = document.getElementById(uid)
+  switch (direction){
+    case 'forward':
+      model.object3D.position.z += 0.1
+      break; 
+    case 'backward':
+      model.object3D.position.z -= 0.1
+      break; 
+    case 'left':
+      model.object3D.position.x -= 0.1
+      break; 
+    case 'right':
+      model.object3D.position.x += 0.1
+      break; 
+    default:
+      console.log('Unable to determin direction: ' + direction);      
+  }   
 }
 
 
@@ -224,3 +182,59 @@ function generateToken() {
   return null; // TODO: add a token generation
 }
 
+function createBroadcaster(streamId) {
+  // create video element
+  var video = document.createElement('video');
+  video.id = 'faceVideo-' + streamId;
+  video.setAttribute('webkit-playsinline', 'webkit-playsinline');
+  video.setAttribute('playsinline', 'playsinline');
+  video.setAttribute('poster', '/imgs/no-video.jpg');
+  console.log(video);
+  // add video object to the DOM
+  document.querySelector('a-assets').appendChild(video);
+
+  // configure the new broadcaster
+  const gltfModel = '#broadcaster';
+  const scale = '-0.55 -0.55 -0.55'; // invert UVs (hack)
+  const offset = (streamCount-1);
+  const position = offset + ' 0 0';
+  const rotation = '180 0 0';
+
+  // create the broadcaster element using the given settings 
+  const parent = document.querySelector('a-marker');
+  var newBroadcaster = document.createElement('a-gltf-model');
+  newBroadcaster.setAttribute('id', streamId);
+  newBroadcaster.setAttribute('gltf-model', gltfModel);
+  newBroadcaster.setAttribute('scale', scale);
+  newBroadcaster.setAttribute('position', position);
+  newBroadcaster.setAttribute('rotation', rotation);
+  parent.appendChild(newBroadcaster);
+
+  console.log(newBroadcaster);
+  // add event listener for model loaded: 
+  newBroadcaster.addEventListener('model-loaded', () => {
+    var mesh = newBroadcaster.getObject3D('mesh');
+    mesh.traverse((node) => {
+      // search the mesh's children for the face-geo
+      if (node.isMesh && node.name == 'face-geo') {
+        // create video texture from video element
+        var texture = new THREE.VideoTexture(video);
+        texture.minFilter = THREE.LinearFilter; 
+        texture.magFilter = THREE.LinearFilter; 
+        texture.flipY = false;
+        // set node's material map to video texture
+        node.material.map = texture
+        node.material.color = new THREE.Color();
+        node.material.metalness = 0;
+      }
+    });
+  }); 
+}
+
+function connectStreamToVideo(agoraStream, video) {
+  video.srcObject = agoraStream.stream;// add video stream to video element as source
+  video.onloadedmetadata = () => {
+    // ready to play video
+    video.play();
+  }
+}
